128c2a641be87583b22d6ba0174982376c75e346
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2019-09-15 14:14:47 +0200

    reponsivness

--- web/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('D:/HUNGRYBEARSTUDIO/WORK/tim-deacon-architect-website/web/node_modules/gatsby-plugin-theme-ui/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
